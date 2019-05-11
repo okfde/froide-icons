7d6d7f66ce9b121f456362cc11d655eef88d2a2c
Johannes Filter <hi@jfilter.de>
2019-05-11 14:59:17 +0200

    add smaller icons

--- create_icons.py
+++ create_icons.py
@@ -16,13 +16,13 @@ add_ons = [Image.open(str(i)) for i in list(p.glob('add-ons/*.png'))]
 for f in p.glob('**/*.png'):
   f.unlink()
 
-img_size = 1000
-add_size = 400
+img_size = 200
+add_size = 80
 
 for i in icons:
   res = i.crop(i.getbbox())
   res = res.resize((img_size, img_size))
-  res.save('dist/' + i.filename.split('/')[-1].replace('.svg', ''))
+  res.save('dist_small/' + i.filename.split('/')[-1].replace('.svg', ''))
 
 for i in icons:
   res = i.crop(i.getbbox())
@@ -31,4 +31,4 @@ for i in icons:
   cos = cos.crop(cos.getbbox())
   cos = cos.resize((add_size, add_size))
   res.paste(cos, box=(img_size - add_size, img_size - add_size), mask=cos)
-  res.save('dist/costs_' + i.filename.split('/')[-1].replace('.svg', ''))
+  res.save('dist_small/costs_' + i.filename.split('/')[-1].replace('.svg', ''))
